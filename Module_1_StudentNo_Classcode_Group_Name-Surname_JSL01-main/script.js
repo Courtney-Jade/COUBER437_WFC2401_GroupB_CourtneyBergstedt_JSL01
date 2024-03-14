0d93af5b9ec632c9e3fa92383cba61519cb13a7b
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-03-14 22:45:07 +0200

    Update script.js
    Added comments and changed a variable

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -5,15 +5,16 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+    
+    // Input field for users to fill in.The input must be filled in a certain way and order
+    let inputField = /^pet_[a-zA-Z0-9]+$/; 
 
-    let petRegex = /^pet_[a-zA-Z0-9]+$/;
-
-       if (petRegex.test(input)) {
-            result = 'Valid Syntax';
+       if (inputField.test(input)) {
+            result = 'Valid Syntax';  //Input field will result as Valid if users have filled it in the correct way
         } else {
-            result = 'Invalid Syntax';
+            result = 'Invalid Syntax'; //Input field will result as Invalid if users have filled it in the incorrect way
         }
-
+              // Displays input results on the web page
             document.getElementById('result').innerText = result;
 }
 
